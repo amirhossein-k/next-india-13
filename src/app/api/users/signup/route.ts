98c60c454af306f4b5b9da3f9b4c6dd5940b8bf9
amirhossein-k <amirhossein.k787878@gmail.com>
2023-08-02 21:36:44 +0330

    1

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,47 @@
+import {ConnectDb} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import {NextRequest, NextResponse} from "next/server";
+import bcryptjs from "bcryptjs";
+
+ConnectDb();
+
+export async function POST(req: NextRequest) {
+  try {
+    const reqBody = await req.json();
+    const {username, email, password} = reqBody;
+
+    console.log(reqBody);
+
+    // check if user aleady exist
+    const user = await User.findOne({email});
+
+    if (user) {
+      return NextResponse.json({error: "User aleady exists"}, {status: 400});
+    }
+
+    // hash password
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log(savedUser, "savedUser");
+
+    return NextResponse.json(
+      {
+        message: "User created successfuly",
+        success: true,
+        savedUser,
+      },
+      {status: 201}
+    );
+  } catch (err: any) {
+    return NextResponse.json({error: err.message}, {status: 500});
+  }
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,22 +1,53 @@
 "use client";
-
+import {useEffect} from "react";
 import axios from "axios";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
+import {toast} from "react-hot-toast/headless";
 
 const SignupPage = () => {
+  const router = useRouter();
+
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const onSignup = async () => {};
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/users/signup", user);
+      console.log(res.data, "success");
+      router.push("/login");
+    } catch (err: any) {
+      console.log("signup failed ", err.message);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-center text-black text-2xl">Signup</h1>
+        <h1 className="text-center text-black text-2xl">
+          {loading ? "procceding.." : "Signup"}
+        </h1>
 
         <hr />
         <label htmlFor="username">username</label>
@@ -51,9 +82,10 @@ const SignupPage = () => {
 
         <button
           onClick={onSignup}
-          className="p-2 border bg-blue-300 border-blue-300 rounded-lg mb-4 focus:outline-none focus:border-gray-500"
+          className={`
+           p-2 border bg-blue-300 border-blue-300 rounded-lg mb-4 focus:outline-none focus:border-gray-500`}
         >
-          signUp
+          {buttonDisabled ? "signup" : "nosignup"}
         </button>
         <Link href={"/login"} className="text-blue-500">
           visit login page
