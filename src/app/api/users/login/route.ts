bebec2596aa70972f2d24346aa686a02c0597d6f
amirhossein-k <amirhossein.k787878@gmail.com>
2023-08-03 17:32:06 +0330

    login done

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,12 +2,62 @@ import {ConnectDb} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import {NextRequest, NextResponse} from "next/server";
 import bcryptjs from "bcryptjs";
+import {error} from "console";
+import {ObjectId} from "mongoose";
+import jwt from "jsonwebtoken";
+ConnectDb();
 
-
-ConnectDb()
-
-export const  POST =async(req:NextRequest)=>{
-    try{}catch(err){
-        
-    }
+interface REQBODY {
+  email: string;
+  password: string;
 }
+interface USER {
+  _id: ObjectId;
+  username: string;
+  email: string;
+  password: string;
+  isVerfied: boolean;
+  isAdmin: boolean;
+}
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const reqBody = await req.json();
+    const {email, password}: REQBODY = reqBody;
+
+    // check exist
+    const user: USER | null = await User.findOne({email});
+    if (!user) {
+      return NextResponse.json({error: "User Not Exist "}, {status: 404});
+    }
+    // check password currect
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({error: "Invalid password"}, {status: 400});
+    }
+    // create tokken data
+    const tokkenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+    // create tokken
+    const tokken = await jwt.sign(tokkenData, process.env.TOKEN_SECTET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json(
+      {
+        message: "Login Successful",
+        success: true,
+      },
+      {status: 201}
+    );
+
+    response.cookies.set("tokken", tokken, {httpOnly: true});
+
+    return response;
+  } catch (err: any) {
+    return NextResponse.json({error: err.message}, {status: 500});
+  }
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,25 +3,41 @@
 import axios from "axios";
 import Link from "next/link";
 import {useRouter} from "next/navigation";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {toast} from "react-hot-toast/headless";
 
 const LoginPage = () => {
+  const router = useRouter();
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
   const [loading, setLoading] = useState(false);
 
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
   const onLogin = async () => {
     try {
       setLoading(true);
-      const res = await axios.
-    } catch (err) {
+      const res = await axios.post("/api/users/login", user);
+      console.log("login success ", res.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (err: any) {
       console.log(err, "login failed");
+      toast.error(err.message);
     } finally {
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -55,7 +71,7 @@ const LoginPage = () => {
           onClick={onLogin}
           className="p-2 border bg-blue-300 border-blue-300 rounded-lg mb-4 focus:outline-none focus:border-gray-500"
         >
-          signUp
+          Login
         </button>
         <Link href={"/signup"} className="text-blue-500">
           visit signup page
